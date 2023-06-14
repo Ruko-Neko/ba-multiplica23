2dc38a055e62520b0607326dab290d97ad045212
dani <dkanemann@gmail.com>
2023-06-13 23:30:37 -0300

    cuerpo

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-alert 
+document.getElementById("formulario").addEventListener("submit"), function(event) {event.preventDefault()};
+
+  let nombre = document.getElementById("nombre").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("msg").value;
+
+
+  if (nombre === '' || email === '' || message === '') {
+    alert("Por favor, complete todos los campos");
+    return;
+  }
+  
